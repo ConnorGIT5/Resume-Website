1b6656f9fd13b474525fd9f8b84c435cd24f095f
conpc <connorroweofficial@gmail.com>
2024-07-27 22:29:39 -0500

    entering a different page with the burger menu will display the 'Home' header each time

--- src/activePage.js
+++ src/activePage.js
@@ -1,6 +1,5 @@
 const activePage = window.location.pathname;
-
-const navLinks = document.querySelectorAll("#nav_a").forEach((link) => {
+const navLinks = document.querySelectorAll(".topnav a").forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
     link.classList.add(`active`);
   }
