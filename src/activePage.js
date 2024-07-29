0d3e5f303d70067b50dda4dd8d5ae283ad272ef4
conpc <connorroweofficial@gmail.com>
2024-07-28 21:58:36 -0500

    1

--- src/activePage.js
+++ src/activePage.js
@@ -1,9 +1,5 @@
 const activePage = window.location.pathname;
 
-setTimeout(function () {
-  //do what you need here
-}, 2000);
-
 const navLinks = document.querySelectorAll(".topnav a").forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
     link.classList.add(`active`);
