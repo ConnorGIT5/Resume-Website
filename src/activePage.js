c95a10bda5b01291c43455af4558aa0f798bbe3b
conpc <connorroweofficial@gmail.com>
2024-07-28 21:52:09 -0500

    add a timeout to delay the active navlink function so I don't get the glitch where are the navlinks are active at once

--- src/activePage.js
+++ src/activePage.js
@@ -1,4 +1,9 @@
 const activePage = window.location.pathname;
+
+setTimeout(function () {
+  //do what you need here
+}, 2000);
+
 const navLinks = document.querySelectorAll(".topnav a").forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
     link.classList.add(`active`);
